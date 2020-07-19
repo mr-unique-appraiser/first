e4629b6688ba197c3d56a2eda0f302ad3107e460
mr-unique-appraiser <68423661+mr-unique-appraiser@users.noreply.github.com>
2020-07-19 22:24:56 +0530

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-print ("qq    qqqq   qq          qq        qq    q  q   qq          qq        qqqq  q  q   qq        qqqq       q  q  q  q   qq        q  q         qqqq  qqqq   qqqqq     qqqq")
+print ("Rakesh")
