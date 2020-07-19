3b3c8df2184f6d0c83be73e230dfb599a5421188
mr-unique-appraiser <68423661+mr-unique-appraiser@users.noreply.github.com>
2020-07-19 22:15:36 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+print ("qq    qqqq   qq          qq
+        qq    q  q   qq          qq
+        qqqq  q  q   qq        qqqq
+        q  q  q  q   qq        q  q
+        qqqq  qqqq   qqqqq     qqqq")
